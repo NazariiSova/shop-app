ff3b58204135614842f27f3a1c1bdf6414fd63e6
NazariiSova <madrigalamar@gmail.com>
2024-12-05 12:01:14 +0200

    redux setup

---
+++ src/redux/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import productsReducer from './features/productsSlice';
+import commentsReducer from './features/commentsSlice';
+
+export const store = configureStore({
+  reducer: {
+    products: productsReducer,
+    comments: commentsReducer
+  }
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
---
+++ src/types/interfaces.ts
@@ -0,0 +1,12 @@
+export interface Comment {
+    id: number;
+    productId: number;
+    content: string;
+    author: string;
+  }
+  export interface Product {
+    id: number;
+    productId: number;
+    content: string;
+    author: string;
+  }
